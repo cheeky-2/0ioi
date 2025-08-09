451d0c9b580a72c9ba24b501ba132f78220b32e7
cheeky-2 <jjuing0@gmail.com>
2025-08-09 19:20:10 +0900

    test2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/0ioi/',
+  base:'/reactoioi/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
